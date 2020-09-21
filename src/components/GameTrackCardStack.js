b6e75cb90f17ca2b91d3eefa42d388e86d2394f7
Braydon Batungbacal <braydon@tryshared.com>
2020-09-21 05:25:40 -0700

    Cards are now swipeable.

--- src/components/GameTrackCardStack.js
+++ src/components/GameTrackCardStack.js
@@ -1,36 +1,75 @@
 import React, { Component } from 'react';
 import { View, PanResponder, Animated, StyleSheet } from 'react-native';
+import { GameTrackCard } from './';
+
+const stackScaleValue = 0.95;
 
 export default class GameTrackCardStack extends Component {
   state = {
-    currentCardIndex: 0,
+    currentTrackIndex: 0,
     panAnimatedValue: new Animated.ValueXY(),
+    stackScaleAnimatedValue: new Animated.Value(stackScaleValue),
   }
 
   panResponder = null;
 
-  componentWillMount() {
+  constructor() {
+    super();
+
     this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponder: () => false,
+      onMoveShouldSetPanResponder: (event, gestureState) => Math.abs(gestureState.dx) > 10,
       onPanResponderMove: Animated.event([
         null,
         {
           dx: this.state.panAnimatedValue.x,
           dy: this.state.panAnimatedValue.y,
         },
-      ]),
-      onPanResponderRelease: () => {
-        Animated.spring(this.state.panAnimatedValue, {
-          toValue: { x: 0, y: 0 },
-          friction: 6,
-        }).start();
-      },
+      ], { useNativeDriver: false }),
+      onPanResponderRelease: this._panResponderRelease,
     });
   }
 
+  _panResponderRelease = (event, gestureState) => {
+    const velocityX = Math.abs(gestureState.vx);
+    const velocityY = Math.abs(gestureState.vy);
+
+    if (velocityX > 0.75 || velocityY > 0.75) {
+      Animated.decay(this.state.panAnimatedValue, {
+        velocity: { x: gestureState.vx, y: gestureState.vy },
+        useNativeDriver: true,
+      }).start();
+
+      Animated.timing(this.state.stackScaleAnimatedValue, {
+        toValue: 1,
+        delay: 100,
+        duration: 400,
+        useNativeDriver: true,
+      }).start();
+
+      setTimeout(() => {
+        this.state.panAnimatedValue.stopAnimation();
+        this.state.panAnimatedValue.setValue({ x: 0, y: 0 });
+        this.state.stackScaleAnimatedValue.setValue(stackScaleValue);
+      }, 500);
+    } else {
+      Animated.spring(this.state.panAnimatedValue, {
+        toValue: { x: 0, y: 0 },
+        friction: 6,
+        useNativeDriver: true,
+      }).start();
+    }
+  }
+
+  _renderCard = card => {
+    return (
+      <GameTrackCard />
+    );
+  }
+
   render() {
-    const { data } = this.props;
-    const { currentCardIndex, panAnimatedValue } = this.state;
+    const { tracks } = this.props;
+    const { currentTrackIndex, panAnimatedValue, stackScaleAnimatedValue } = this.state;
     const panStyle = { transform: panAnimatedValue.getTranslateTransform() };
 
     return (
@@ -39,12 +78,15 @@ export default class GameTrackCardStack extends Component {
           {...this.panResponder.panHandlers}
           style={[ styles.cardContainer, panStyle ]}
         >
-          {this.props.renderCard(data[currentCardIndex])}
+          {this._renderCard(tracks[currentTrackIndex])}
         </Animated.View>
 
-        <View style={styles.stackCardContainer}>
-          {this.props.renderCard(data[currentCardIndex + 1])}
-        </View>
+        <Animated.View style={[
+          styles.stackCardContainer,
+          { transform: [ { scale: stackScaleAnimatedValue } ] },
+        ]}>
+          {this._renderCard(tracks[currentTrackIndex + 1])}
+        </Animated.View>
       </View>
     );
   }
