1a5b5af90e08354dbfa78a550fec4f9b39bc27fe
Braydon Batungbacal <braydon@tryshared.com>
2020-10-15 03:10:28 -0700

    Added notification indicator to "upload your first track"

--- src/components/UploadTrackCard.js
+++ src/components/UploadTrackCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Button from './Button';
 import Card from './Card';
 import maestro from '../maestro';
@@ -14,7 +14,10 @@ export default class UploadTrackCard extends Component {
   render() {
     return (
       <Card style={styles.container}>
-        <Text style={styles.titleText}>Upload Your First Track</Text>
+        <View>
+          <Text style={styles.titleText}>Upload Your First Track</Text>
+          <View style={styles.notificationBubble} />
+        </View>
 
         <Button onPress={this._openUploadTrack} style={styles.button}>Upload Audio File</Button>
         <Button onPress={this._openUploadTrack} style={styles.button}>Import From SoundCloud</Button>
@@ -31,6 +34,17 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     padding: 32,
+  },
+  notificationBubble: {
+    backgroundColor: '#FF0000',
+    borderColor: '#FFFFFF',
+    borderRadius: 12,
+    borderWidth: 2,
+    height: 12,
+    position: 'absolute',
+    right: -12,
+    top: -4,
+    width: 12,
   },
   titleText: {
     color: '#04112A',
